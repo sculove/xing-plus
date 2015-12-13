0da94db5dfb16af4b96e96e22ea8b432ffaf8ba6
sculove <sculove@gmail.com>
2015-12-14 01:41:47 +0900

    change addTarget interface add run method

--- xing/xareal.py
+++ xing/xareal.py
@@ -40,13 +40,16 @@ class Real(threading.Thread):
 
     # 타겟을 등록한다. 여기서 타겟은 모니터링하는 대상
     # 예) 특정 종목을 추가로 등록
-    def addTarget(self, param = {}):
-        if isinstance(param, dict):
-            param = [ param ]
-        for t in param:
-            for v in t.keys():
-                self.real.SetFieldData("InBlock", v, t[v])
+    def addTarget(self, value = None, name = "shcode"):
+        if value == None:
+            self.removeAllTargets()
             self.real.AdviseRealData()
+        else:
+            if isinstance(value, str):
+                value = [ value ]
+            for v in value:
+                self.real.SetFieldData("InBlock", name, v)
+                self.real.AdviseRealData()
         return self
 
     # 모든 타겟의 모니터링을 제거한다.
@@ -54,7 +57,7 @@ class Real(threading.Thread):
         self.real.UnadviseRealData()
 
     # 특정 타겟의 모니터링을 제거한다.
-    def removeTargets(self, key):
+    def removeTarget(self, key):
         self.real.UnAdviseRealDataWithKey(key)
 
     def run(self):
@@ -65,10 +68,41 @@ class Real(threading.Thread):
 
 '''
 manager = RealManager()
-manager.addTask("SC1", ("eventid", "ordxctptncode", "ordmktcode", "ordptncode", "mgmtbrnno",
-                    "accno1","Isuno", "Isunm", "ordno", "orgordno", "execno",
-                    "ordqty", "ordprc", "execqty", "execprc", "ordtrxptncode",
-                    "secbalqty", "avrpchsprc", "pchsant"), 100).addTarget().start()
+
+# 주문 체결
+manager.addTask("SC1", ("Isuno", "Isunm", "ordno", "orgordno",
+                     "eventid", "ordxctptncode", "ordmktcode",
+                     "ordptncode", "mgmtbrnno",  "accno1",
+                     "execno", "ordqty", "ordprc", "execqty",
+                     "execprc", "ordtrxptncode", "secbalqty",
+                     "avrpchsprc", "pchsant"), 50).addTarget()
+# 코스피 호가
+manager.addTask("H1_", ("shcode", "hottime","totofferrem", "totbidrem",
+            "offerho1", "bidho1", "offerrem1", "bidrem1",
+            "offerho2", "bidho2", "offerrem2", "bidrem2",
+            "offerho3", "bidho3", "offerrem3", "bidrem3",
+            "offerho4", "bidho4", "offerrem4", "bidrem4",
+            "offerho5", "bidho5", "offerrem5", "bidrem5",
+            "offerho6", "bidho6", "offerrem6", "bidrem6",
+            "offerho7", "bidho7", "offerrem7", "bidrem7",
+            "offerho8", "bidho8", "offerrem8", "bidrem8",
+            "offerho9", "bidho9", "offerrem9", "bidrem9",
+            "offerho10", "bidho10", "offerrem10", "bidrem10"
+        ), 100).addTarget("004170")
+
+# 코스닥 호가
+manager.addTask("HA_", ("shcode", "hottime","totofferrem", "totbidrem",
+            "offerho1", "bidho1", "offerrem1", "bidrem1",
+            "offerho2", "bidho2", "offerrem2", "bidrem2",
+            "offerho3", "bidho3", "offerrem3", "bidrem3",
+            "offerho4", "bidho4", "offerrem4", "bidrem4",
+            "offerho5", "bidho5", "offerrem5", "bidrem5",
+            "offerho6", "bidho6", "offerrem6", "bidrem6",
+            "offerho7", "bidho7", "offerrem7", "bidrem7",
+            "offerho8", "bidho8", "offerrem8", "bidrem8",
+            "offerho9", "bidho9", "offerrem9", "bidrem9",
+            "offerho10", "bidho10", "offerrem10", "bidrem10"
+        ), 100).addTarget("168330")
 '''
 class RealManager:
     def __init__(self):
@@ -81,12 +115,14 @@ class RealManager:
         realTask = Real(type, outputStyle, queue)
         self.tasks[type] = realTask
         self.queues[type] = queue
+        realTask.start()
         return realTask
 
     # 작업을 제거한다.
     def removeTask(self, type):
         task = self.getTask(type)
         if task:
+            task.removeAllTargets()
             # @todo check 큐에 있는걸 다 비우고 할것인가?
             task.running = False
             del self.queues[type]
@@ -97,3 +133,14 @@ class RealManager:
 
     def getQueue(self, type):
         return self.queues[type] if type in self.queues else None
+
+    # 큐에 있는 정보를 callback 함수로 호출해줌
+    def run(self, cb = None):
+        for k,v in self.queues.items():
+            data = []
+            queue = self.getQueue(k)
+            if queue and queue.qsize() > 0:
+                for i in range(queue.qsize()):
+                    data.append(queue.get())
+            if cb:
+                cb(k, data)
