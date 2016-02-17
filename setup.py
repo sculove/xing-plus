1f6642aaea5aa385bbaedd0fca87f8b1aa9a1432
sculove <sculove@gmail.com>
2016-02-18 02:46:02 +0900

    upgrade version for pypi

--- setup.py
+++ setup.py
@@ -1,26 +1,27 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
-# https://pythonhosted.org/setuptools/setuptools.html#building-and-distributing-packages-with-setuptools
 # http://blog.dokenzy.com/archives/986
+# https://pythonhosted.org/setuptools/setuptools.html#building-and-distributing-packages-with-setuptools
+# http://python-packaging-user-guide.readthedocs.org/en/latest/distributing/#name
 # http://stackoverflow.com/questions/7522250/how-to-include-package-data-with-setuptools-distribute
 
 setup(
     name="xing-plus",
-    version="1.0.0",
+    version="1.0.1",
     license="MIT License",
     author="sculove",
     author_email="sculove@gmail.com",
     url="https://github.com/sculove/xing-plus",
     keywords=["xing","stock","systemtrading"],
-    platform="win32",
     description="more easy eBEST INVESTMENT API",
     long_description="xing plus supports more easy api for eBEST INVESTMENT",
     classifiers=[
         "Environment :: Win32 (MS Windows)",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3"
+        "Programming Language :: Python :: 3.5",
+        "Natural Language :: Korean"
     ],
     packages=find_packages(),
     package_data={"": ["*.res"]},
