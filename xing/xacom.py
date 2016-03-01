321891b4241c04072f782a1dceb6f5a16b709371
sculove <sculove@gmail.com>
2016-03-02 07:42:13 +0900

    remove util file

--- xing/xacom.py
+++ xing/xacom.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
+from datetime import datetime, timedelta
+import math
+import pandas
+
 def parseErrorCode(code):
     """에러코드 메시지
 
@@ -130,6 +134,7 @@ def parseJstatus(jstatus):
     }
     return ht[jstatus] if jstatus in ht else ""
 
+
 def parseMarket(jangubun):
     """장 구분
 
@@ -149,3 +154,157 @@ def parseMarket(jangubun):
         "8" : "EUREX야간옵션선물"
     }
     return ht[jangubun] if jangubun in ht else ""
+
+
+def timeType(base = None):
+	"""장 전,후 시간을 반환
+
+		:param base: 기준일시
+		:type base: datetime
+		:return: 기준일시에 맞는 타입문자를 반환
+
+			BEFORE(장시작 전),SHOWTIME(장 운영시간),AFTER(장종료 후)
+
+		::
+
+			timeType()
+			timeType(datetime.today())
+	"""
+	today = base if base else datetime.today()
+	mainStart = today.replace(hour=8, minute=50, second=0, microsecond=0)
+	mainEnd = today.replace(hour=15, minute=0, second=0, microsecond=0)
+	if today.weekday() < 5:
+		if today >= mainStart and today <= mainEnd:
+			return "SHOWTIME"
+		else:
+			if today < mainStart:
+				return "BEFORE"
+			elif today > mainEnd:
+				return "AFTER"
+	else:
+		return "NONE"
+
+
+def today():
+	"""오늘 날자를 yyyymmdd 형태로 반환
+
+		::
+
+			today()	# 20160101
+	"""
+	return datetime.today().strftime("%Y%m%d")
+
+
+def latestBusinessDay():
+	"""가장 최근 영업일을 yyyymmdd 형태로 반환
+
+		::
+
+			latestBusinessDay()	# 20160104
+	"""
+	baseday = datetime.today()
+	if baseday.weekday() > 4:
+		while baseday.weekday() > 4:
+			baseday = baseday - timedelta(days=1)
+	return baseday.strftime("%Y%m%d")
+
+
+
+# def printMax(x):
+# 	pandas.set_option("display.max_rows", len(x))
+# 	pandas.set_option("display.max_columns", len(x.columns))
+# 	print(x)
+# 	pandas.reset_option("display.max_rows")
+# 	pandas.reset_option("display.max_columns")
+#
+# def split(arr, size):
+# 	arrs = []
+# 	while len(arr) > size:
+# 		pice = arr[:size]
+# 		arrs.append(pice)
+# 		arr = arr[size:]
+# 	arrs.append(arr)
+# 	return arrs
+
+# # 호가 단위
+# def callValueUnit(price, isKospi = False):
+# 	unit = None
+# 	price = int(price)
+# 	if price < 1000:
+# 		unit = 1
+# 	elif price >= 1000 and price < 5000:
+# 		unit = 5
+# 	elif price >= 5000 and price < 10000:
+# 		unit = 10
+# 	elif price >= 10000 and price < 50000:
+# 		unit = 50
+# 	elif price >= 50000:
+# 		if isKospi:
+# 			if price < 100000:
+# 				unit = 100
+# 			elif price >= 100000 and price < 500000:
+# 				unit = 500
+# 			elif price >= 500000:
+# 				unit = 1000
+# 		else:
+# 			unit = 100
+# 	return unit
+#
+# # 구분
+# def sign(type):
+# 	result = None
+# 	type = int(type)
+# 	if type < 3:
+# 		#상승
+# 		result = 1
+# 	elif type == 3:
+# 		#보합
+# 		result = 0
+# 	elif type > 3:
+# 		#하락
+# 		result = -1
+# 	return result
+#
+# # candle
+# def candle(price, open, high, low):
+# 	# print(price, open, high, low)
+# 	p = int(price)
+# 	o = int(open)
+# 	h = int(high)
+# 	l = int(low)
+# 	height = h-l
+# 	body = 0 if height == 0 else round((p-o)/height,2)
+#
+# 	if body > 0:
+# 		#양봉
+# 		type = 1
+# 		top = (h-p)/height
+# 		bottom = (o-l)/height
+# 	elif body < 0:
+# 		#음봉
+# 		type = -1
+# 		top = (h-o)/height
+# 		bottom = (p-l)/height
+# 	else:
+# 		#보합
+# 		type = 0.0
+# 		top = 0.0
+# 		bottom = 0.0
+#
+# 	return {
+# 		"type" : type,
+# 		"top" : round(top,2) * 100,
+# 		"bottom" : round(bottom,2) * 100,
+# 		"body" : math.fabs(body) * 100
+# 	}
+#
+# def profit(buy, sell):
+# 	#매매수수료
+# 	fee = (float(buy) * 0.00015) + (float(sell) * 0.00315)
+# 	profit = sell - buy - fee
+#
+# 	return {
+# 		"profit" : profit,
+# 		"rate" : round(profit/buy * 100,2)
+# 	}
+
--- xing/xareal.py
+++ xing/xareal.py
@@ -24,11 +24,11 @@ class _XARealEvents:
 
     def OnReceiveRealData(self, szTrCode):
         log.debug(" - OnReceiveRealData (%s)" % szTrCode )
+        log.debug(self._putData(szTrCode))
         self.queue.put(self._putData(szTrCode))
 
 class Real(threading.Thread):
     """실시간 TR을 모니터링하는 작업 클래스
-
         :param type: 실시간 TR 코드
         :type type: str
         :param outputStyle: 실시간 TR 반환 데이터 컬럼
@@ -36,9 +36,7 @@ class Real(threading.Thread):
         :param queue: 실시간 TR 반환 데이터를 저장할 큐 객체
         :type queue: Queue
         :return: self
-
         ::
-
             real = Real("SC1", ("eventid", "ordxctptncode", "ordmktcode", "ordptncode", "mgmtbrnno",
                 "accno1","Isuno", "Isunm", "ordno", "orgordno", "execno",
                 "ordqty", "ordprc", "execqty", "execprc", "ordtrxptncode",
@@ -54,20 +52,15 @@ class Real(threading.Thread):
 
     def addTarget(self, value = None, name = "shcode"):
         """타겟을 등록한다. 여기서 타겟은 모니터링하는 대상이다.
-
             :param value: 모니터링할 대상. 기본값은 None
             :type value: None, str
             :param name: 모니터링할 대상의 속성. 기본값은 "shcode"
             :type name: str
             :return: self
-
             .. note::
-
                 - 타겟에 정보를 전달할 필요가 없는 '주문 체결'과 같은 작업도 addTarget를 호출하여 모니터링을 시작해야한다.
                 - '코스피 호가'는 타겟(종목코드(shcode))를 추가로 여러개 모니터링 할 수 있다.
-
             ::
-
                 real.addTarget()
                 real.addTarget("005930")
                 real.addTarget(["005930","035420"])
@@ -86,9 +79,7 @@ class Real(threading.Thread):
 
     def removeAllTargets(self):
         """모든 타겟의 모니터링을 제거한다.
-
             ::
-
                 real.removeAllTargets()
         """
         self.real.UnadviseRealData()
@@ -96,21 +87,16 @@ class Real(threading.Thread):
 
     def removeTarget(self, key):
         """특정 타겟의 모니터링을 제거한다.
-
             :param key: 특정 타겟
             :type key: str
-
             ::
-
                 real.removeAllTargets()
         """
         self.real.UnAdviseRealDataWithKey(key)
 
     def run(self):
         """실시간 TR을 모니터링 한다
-
             ::
-
                 real.run()
         """
         while self.running:
@@ -120,9 +106,7 @@ class Real(threading.Thread):
 
 class RealManager:
     """실시간 TR을 모니터링하는 작업 클래스(Real)을 관리하는 클래스
-
     ::
-
         manager = RealManager()
     """
     def __init__(self):
@@ -131,7 +115,6 @@ class RealManager:
 
     def addTask(self, type, outputStyle, maxQueue):
         """실시간 작업을 추가한다
-
             :param type: 실시간 TR 코드
             :type type: str
             :param outputStyle: 실시간 TR 반환 데이터 컬럼
@@ -139,9 +122,7 @@ class RealManager:
             :param maxQueue: 실시간 TR 반환 데이터를 저장할 큐의 개수
             :type maxQueue: int
             :return: Real
-
         ::
-
             # 주문 체결
             manager.addTask("SC1", ("Isuno", "Isunm", "ordno", "orgordno",
                      "eventid", "ordxctptncode", "ordmktcode",
@@ -149,7 +130,6 @@ class RealManager:
                      "execno", "ordqty", "ordprc", "execqty",
                      "execprc", "ordtrxptncode", "secbalqty",
                      "avrpchsprc", "pchsant"), 50).addTarget()
-
             # 코스피 호가
             manager.addTask("H1_", ("shcode", "hottime","totofferrem", "totbidrem",
                         "offerho1", "bidho1", "offerrem1", "bidrem1",
@@ -163,7 +143,6 @@ class RealManager:
                         "offerho9", "bidho9", "offerrem9", "bidrem9",
                         "offerho10", "bidho10", "offerrem10", "bidrem10"
                     ), 100).addTarget(["005930","035420"])
-
             # 코스닥 호가
             manager.addTask("HA_", ("shcode", "hottime","totofferrem", "totbidrem",
                         "offerho1", "bidho1", "offerrem1", "bidrem1",
@@ -187,12 +166,9 @@ class RealManager:
 
     def removeTask(self, type):
         """실시간 작업을 제거한다.
-
             :param type: 실시간 TR 코드
             :type type: str
-
         ::
-
             manager.removeTask("SC1")
         """
         task = self.getTask(type)
@@ -205,44 +181,33 @@ class RealManager:
 
     def getTask(self, type):
         """실시간 작업을 얻는다.
-
             :param type: 실시간 TR 코드
             :type type: str
             :return: 실시간 작업 객체
             :rtype: Real
-
         ::
-
             manager.getTask("SC1")
         """
         return self.tasks[type] if type in self.tasks else None
 
     def getQueue(self, type):
         """실시간 작업의 큐를 얻는다.
-
             :param type: 실시간 TR 코드
             :type type: str
             :return: 실시간 작업 객체의 큐
             :rtype: Queue
-
         ::
-
             manager.getQueue("SC1")
         """
         return self.queues[type] if type in self.queues else None
 
     def run(self, cb = None):
         """실시간 TR별 큐의 정보를 추출하여 callback 함수로 전달한다.
-
             :param cb: 큐에서 추출된 정보를 받을 callback 함수
             :type type: def
-
             .. note::
-
                 callback 함수는 type(실시간 TR코드)와 data(실시간 TR의 outputStyle의 데이터)를 파라미터로 갖는다.
-
         ::
-
             def callback(type, data):
                 for i in range(len(data)):
                     if type == "SC1":
@@ -251,7 +216,6 @@ class RealManager:
                         # ...
                     else:
                         # ...
-
             manager.run(callback)
         """
         for k,v in self.queues.items():
